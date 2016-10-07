965acf0fee4b0b48b58cba188aa28cb012444c30
Nicky Kwan <nickykwn@gmail.com>
2016-10-07 14:40:05 -0400

    added gameboard and disc spots

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,36 @@
-var connect4 = createArray();
-function createArray() {
-  var board = [];
-  for(let i = 0; i<6; i++){
-    board[i] = [];
-    for(let j = 0; j<7; j++){
-      board[j].push('i');
-      board[j].click(dropDisk);
-    }
+console.log('It has loaded!');
+// var connect4 = createArray();
+// function createArray() {
+//   var board = [];
+//   for(let i = 0; i<6; i++){
+//     board[i] = [];
+//     for(let j = 0; j<7; j++){
+//       board[j].push('*');
+
+//     }
+//   }
+//   return board;
+// }
+function switchTurns(previousColor) {
+  if (previousColor === 'red') {
+    whoisTurnIsIt = 'black'
+  } else {
+    whoisTurnIsIt = 'red';
   }
-  return board;
+  document.getElementsByClassName('turnBox')[0].innerHTML = whoisTurnIsIt;
 }
 
+var $color = $('.spots');
+if(whoisTurnIsIt === 'red') {
+  changeBackgroundToRed();
+} else {
+  changeBackgroundToBlack
+}
+
+function changeBackgroundToRed() {
+  event.target.style.backgroundColor = 'red';
+}
+
+function changeBackgroundToBlack() {
+  event.target.style.backgroundColor = 'black';
+}
