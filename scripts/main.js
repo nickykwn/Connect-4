b2b8147c268dc96e70d9a2cd8e45227344b95b5b
Nicky Kwan <nickykwn@gmail.com>
2016-10-10 22:36:30 -0400

    cleaned up unused code

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 console.log('It has loaded!');
+// variables set globally before they are called upon in functions
 var whoseTurnIsIt = 'red';
 var board = $('.spot');
+
+// function to add click event for all the spots on the connect 4 board
+//
 $('.spot').on('click', function(event) {
   var element = $(event.target);
     var col = element.attr('data-column');
@@ -37,41 +41,3 @@ function createArray() {
   }
 }
 
-//Player function to drop a colored Disc down and check if the row below it is filled
-// function dropDisc(color, columnIndex) {
-//   function tryDropDisc(columnIndex, rowIndex) {
-//     if (board['column'+columnIndex]['row'+rowIndex]['value'] === 'empty') {
-//       board['column'+columnIndex]['row'+rowIndex]['value'] = color;
-//       switchTurns(color);
-//     } else {
-//       tryDropDisc(columnIndex, rowIndex-1);
-//     }
-//   } x6
-//   tryDropDisc(columnIndex, 5)
-// }
-
-//checks whose turn it is and displays the color of whose turn it is in the box
-// function switchTurns(previousColor) {
-//   if (previousColor === 'red') {
-//     whoseTurnIsIt = changeBackgroundToBlack();
-//   } else {
-//     whoseTurnIsIt = changeBackgroundToRed();
-//   }
-//   $('.turnBox').html(whoseTurnIsIt);
-// }
-
-// var $color = $('.spots');
-// if(whoisTurnIsIt === 'red') {
-//   changeBackgroundToRed();
-// } else {
-//   changeBackgroundToBlack();
-// }
-
-// //functions to change the background of spots on the board
-// function changeBackgroundToRed() {
-//   event.target.style.backgroundColor = 'red';
-// }
-
-// function changeBackgroundToBlack() {
-//   event.target.style.backgroundColor = 'black';
-// }
